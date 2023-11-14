7ea8b2427ad9e711b95d1a54e84147b9bd83a771
Knutnoh <69119941+Knutnoh@users.noreply.github.com>
2023-11-14 22:21:12 +0100

    Update autoload.php

--- Lichtsteuerung/helper/autoload.php
+++ Lichtsteuerung/helper/autoload.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-foreach (glob(__DIR__ . '/*.php') as $filename) {
-    if (basename($filename) != 'autoload.php') {
+foreach (glob(__DIR__ . DIRECTORY_SEPARATOR . '*.php') as $filename) {
+    if (basename($filename) !== 'autoload.php') {
         include_once $filename;
     }
 }
